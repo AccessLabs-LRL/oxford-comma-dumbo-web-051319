f4a496be9799d8349e2018b9a1dd819802659a50
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2021-01-18 17:54:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array == 2 
     return "#{array[0]} and #{array[1]}"
   
-  elsif array <= 2 
+  elsif array < arra2 
     array[-1].shift("and")
   end
   array.join(", ")
